9fc6e9dbc3197535591864955beca275832f0d5b
LucasK <lucas.kuntz@afpa-dev-pompey.fr>
2024-11-15 13:16:13 +0100

    Script
    Modification of script to run sql files to define password in config (no more warning) + MySQL commands with default character set to avoid garbled strings.

--- sql/init.sh
+++ sql/init.sh
@@ -1,23 +1,33 @@
+# mysql_config_editor set --login-path=client --host=localhost --user=root --port=3306 --password
 echo "main :"
 echo "0 >"
-mysql -u root --password= ffb_main < main/0.main_drop.sql
+mysql --login-path=client ffb_main < main/0.main_drop.sql
+echo "done"
 echo "1 >"
-mysql -u root --password= ffb_main < main/1.main_struct.sql
+mysql --login-path=client ffb_main < main/1.main_struct.sql
+echo "done"
 echo "2 >"
-mysql -u root --password= ffb_main < main/2.main_data_params.sql
+mysql --login-path=client --default-character-set=utf8  ffb_main < main/2.main_data_params.sql
+echo "done"
 echo "3 >"
-mysql -u root --password= ffb_main < main/3.main_data_fanfictions.sql
+mysql --login-path=client --default-character-set=utf8  ffb_main < main/3.main_data_fanfictions.sql
+echo "done"
 echo "4 >"
-mysql -u root --password= ffb_main < main/4.main_data_links.sql
+mysql --login-path=client --default-character-set=utf8  ffb_main < main/4.main_data_links.sql
+echo "done"
 echo "5 >"
-mysql -u root --password= ffb_main < main/5.main_data_series.sql
+mysql --login-path=client --default-character-set=utf8  ffb_main < main/5.main_data_series.sql
+echo "done"
 
 echo "stats :"
 echo "0 >"
-mysql -u root --password= ffb_stats < stats/0.stats_drop.sql
+mysql --login-path=client ffb_stats < stats/0.stats_drop.sql
+echo "done"
 echo "1 >"
-mysql -u root --password= ffb_stats < stats/1.stats_struct.sql
+mysql --login-path=client ffb_stats < stats/1.stats_struct.sql
+echo "done"
 
 echo "tests :"
-mysql -u root --password= ffb_tests < main/0.main_drop.sql
-mysqldump -u root --password= ffb_main | mysql -u root --password= ffb_tests
+mysql --login-path=client ffb_tests < main/0.main_drop.sql
+mysqldump --login-path=client ffb_main | mysql --login-path=client ffb_tests
+echo "done"
