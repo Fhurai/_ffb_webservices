02fa2b853bea959f78ce843b809e4db6fa391eb9
Fhurai <kulu57@gmail.com>
2024-12-20 19:26:38 +0100

    Upgraded tests results presentation

--- tests/index.php
+++ tests/index.php
@@ -24,40 +24,43 @@ $complex = new ComplexEntitiesTests();
 <body>
     <h2>DB Tests</h2>
     <p>All tests about DB accesses</p>
-    <?php foreach(["Parameters" => $parameters, "Entities" => $entities, "Named entities" => $named, "Complex entities" => $complex] as $key => $test): ?>
-    <table>
-        <thead class="<?= $test->testCheck() ? "success" : "fail" ?>">
-            <tr>
-                <th colspan="4">
-                    Total <?= $key ?> checks :
-                    <?= $test->testCheck() ? "All tests succeed !" : "At least one test failed !" ?>
-                </th>
-            </tr>
-            <tr>
-                <th>Class</th>
-                <th>Method</th>
-                <th>Subject</th>
-                <th>Success</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($test->getChecksArray() as $libelle => $value): ?>
-                <tr class="<?= $value ? "success" : "fail" ?>">
-                    <?php $libelleParts = explode("_", $libelle); ?>
-                    <td><?= $libelleParts[0] ?></td>
-                    <td><?= $libelleParts[1] ?></td>
+    <?php foreach (["Parameters" => $parameters, "Entities" => $entities, "Named entities" => $named, "Complex entities" => $complex] as $key => $test): ?>
+        <div class="dropdown-panel">
+            <label for="toggle<?= $key ?>" class="<?= $test->testCheck() ? "success" : "fail" ?>">
+                Total <?= $key ?> checks :
+                <?= $test->testCheck() ? count($test->getChecksArray()) . " tests succeed !" : "At least one test failed !" ?>
+            </label>
+            <input id="toggle<?= $key ?>" type="checkbox" />
+            <div class="content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Class</th>
+                            <th>Method</th>
+                            <th>Subject</th>
+                            <th>Success</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($test->getChecksArray() as $libelle => $value): ?>
+                            <tr class="<?= $value ? "success" : "fail" ?>">
+                                <?php $libelleParts = explode("_", $libelle); ?>
+                                <td><?= $libelleParts[0] ?></td>
+                                <td><?= $libelleParts[1] ?></td>
 
-                    <?php
-                    unset($libelleParts[1]);
-                    unset($libelleParts[0]);
-                    $newLibelle = implode("_", $libelleParts);
-                    ?>
-                    <td><?= $newLibelle ?></td>
-                    <td><?= $value ? "Success" : "Fail" ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+                                <?php
+                                unset($libelleParts[1]);
+                                unset($libelleParts[0]);
+                                $newLibelle = implode("_", $libelleParts);
+                                ?>
+                                <td><?= $newLibelle ?></td>
+                                <td><?= $value ? "Success" : "Fail" ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     <?php endforeach; ?>
 </body>
 
