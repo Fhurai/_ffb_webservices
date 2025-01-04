3537cbdf3ec404db01e9ba74d009ebc1f5f575e4
Fhurai <kulu57@gmail.com>
2025-01-04 16:38:29 +0100

    Documentation Evaluable trait

--- src/entity/Evaluable.php
+++ src/entity/Evaluable.php
@@ -1,26 +1,55 @@
 <?php
 
-trait Evaluable {
+/**
+ * Evaluable trait
+ */
+trait Evaluable
+{
+    /**
+     * Evaluable identifier.
+     * @var int
+     */
+    private ?int $score_id;
+    /**
+     * Evaluable evaluation.
+     * @var string
+     */
+    private ?string $evaluation;
 
-    private int $score_id;
-    private string $evaluation;
-
-    public function getScoreId(): int
+    /**
+     * Getter Score_id
+     * @return ?int Score_id
+     */
+    public function getScoreId(): ?int
     {
         return $this->score_id;
     }
 
-    public function setScoreId(int $score_id): void
+    /**
+     * Setter Score_id
+     * @param int $score_id New Score_id
+     * @return void
+     */
+    public function setScoreId(int $score_id = null): void
     {
         $this->score_id = $score_id;
     }
 
+    /**
+     * Getter Evaluation.
+     * @return string Evaluation.
+     */
     public function getEvaluation(): string
     {
         return $this->evaluation;
     }
 
-    public function setEvaluation(string $evaluation): void
+    /**
+     * Setter Evaluation.
+     * @param string $evaluation New Evaluation.
+     * @return void
+     */
+    public function setEvaluation(string $evaluation = null): void
     {
         $this->evaluation = $evaluation;
     }
