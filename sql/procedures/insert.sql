7e6095745dc1dd60458d681bbe7d939c82768f84
Fhurai <kulu57@gmail.com>
2025-01-17 21:37:43 +0100

    Insert procedures for tags & characters

--- sql/procedures/insert.sql
+++ sql/procedures/insert.sql
@@ -59,9 +59,75 @@ BEGIN
         SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Language already exists.';
     END IF;
     COMMIT;
-END $$
+END; $$
 -- Tags
+DROP FUNCTION IF EXISTS fn_check_tagtype $$
+CREATE FUNCTION fn_check_tagtype(TID INT) RETURNS BOOLEAN
+BEGIN
+    DECLARE exist BOOLEAN DEFAULT true;
+        SELECT COUNT(*) > 0 INTO exist FROM `tag_types` WHERE id = TID;
+    RETURN exist;
+END;$$
+
+DROP PROCEDURE IF EXISTS prc_insert_tags $$
+CREATE PROCEDURE prc_insert_tags
+(IN label VARCHAR(100), IN descrp TEXT, IN typeTag INT, OUT id INT)
+BEGIN
+    DECLARE t_count INT DEFAULT 1;
+    START TRANSACTION;
+    IF fn_check_tagtype(typeTag) THEN
+        SET t_count = (SELECT COUNT(*) 
+                    FROM `tags`
+                    WHERE name = label 
+                    AND type_id = typeTag
+                    FOR SHARE);
+        IF t_count = 0 THEN
+            INSERT INTO `tags` (`name`, `description`, `type_id`) VALUES (label, descrp, typeTag);
+            SET id = LAST_INSERT_ID();
+        ELSE
+            ROLLBACK;
+            SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Tag already exists.';
+        END IF;
+    ELSE
+        ROLLBACK;
+        SIGNAL SQLSTATE '50002' SET MESSAGE_TEXT = 'Type tag does not exists.';
+    END IF;
+    COMMIT;
+END$$
 -- Characters
+DROP FUNCTION IF EXISTS fn_check_fandom $$
+CREATE FUNCTION fn_check_fandom(FID INT) RETURNS BOOLEAN
+BEGIN
+    DECLARE exist BOOLEAN DEFAULT true;
+        SELECT COUNT(*) > 0 INTO exist FROM `fandoms` WHERE id = FID;
+    RETURN exist;
+END;$$
+
+DROP PROCEDURE IF EXISTS prc_insert_characters $$
+CREATE PROCEDURE prc_insert_characters
+(IN label VARCHAR(100), IN identifier INT, OUT id INT)
+BEGIN
+    DECLARE f_count INT DEFAULT 1;
+    START TRANSACTION;
+    IF fn_check_fandom(identifier) THEN
+        SET f_count = (SELECT COUNT(*) 
+                    FROM `characters`
+                    WHERE name = label 
+                    AND fandom_id = identifier
+                    FOR SHARE);
+        IF f_count = 0 THEN
+            INSERT INTO `characters` (`name`, `fandom_id`) VALUES (label, identifier);
+            SET id = LAST_INSERT_ID();
+        ELSE
+            ROLLBACK;
+            SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Character already exists.';
+        END IF;
+    ELSE
+        ROLLBACK;
+        SIGNAL SQLSTATE '50002' SET MESSAGE_TEXT = 'Fandom does not exists.';
+    END IF;
+    COMMIT;
+END$$
 -- Relations
 -- Fanfictions
 -- Links
