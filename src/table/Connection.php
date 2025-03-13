b3bca5d3bb6d456a388ab1bdc1059863dab6c1cf
LucasK <lucas.kuntz@afpa-dev-pompey.fr>
2025-03-13 16:55:04 +0100

    Rework tests and tables with new knowledge

--- src/table/Connection.php
+++ src/table/Connection.php
@@ -47,16 +47,6 @@ class Connection
      */
     private PDO $db;
     /**
-     * Table name.
-     * @var 
-     */
-    private string $table;
-    /**
-     * Array of columns names.
-     * @var 
-     */
-    private array $columns;
-    /**
      * Label of connection type.
      * @var string
      */
@@ -68,183 +58,54 @@ class Connection
     private $user;
 
     /**
-     * Constructor.
-     * @param string $typeConnection Connection to use [main/stats/tests].
-     * @param string $user SQL user.
+     * Get the database connection.
+     * @param string $typeConnection
+     * @param string $user
+     * @return PDO
+     * @throws FfbTableException
      */
-    public function __construct(string $typeConnection, string $user)
+    public static function getDatabase(string $typeConnection, string $user): PDO
     {
-        if (in_array($typeConnection, ["main", "stats", "tests"])) {
+        static $dbInstances = [];
 
-            if (in_array($user, ["guest", "user", "admin"])) {
-                // Set the type of connection.
-                $this->typeConnection = $typeConnection;
+        $key = $typeConnection . '_' . $user;
 
-                // Set the user of connection.
-                $this->user = $user;
+        if (!isset($dbInstances[$key])) {
+            $dbInstances[$key] = self::createDbConnection($typeConnection, $user);
+        }
 
-                // If connection type is known, then use config file.
-                if (file_exists("../config/config.php"))
-                    $configFile = include "../config/config.php";
-                else if (file_exists("../../config/config.php"))
-                    $configFile = include "../../config/config.php";
+        return $dbInstances[$key];
+    }
 
-                try {
-
-                    // Creation of Php Database Object with provided data from config.
-                    $this->db = new PDO("mysql:host=" . $configFile["credentials"][$user]["host"] . ";dbname=" . $configFile["db"][$typeConnection], $configFile["credentials"][$user]["user"], $configFile["credentials"][$user]["password"]);
-                } catch (PDOException $e) {
-
-                    // Exception caught, throw new exception with previous exception message.
-                    throw new FfbTableException($e->getMessage());
-                }
-
-                // Auto set of name of table.
-                $this->setTable($this->getNameTable());
-
-                // Auto set columns of table.
-                $this->setColumns($this->getPropertiesColumns());
-            } else {
-                // Unknown sql user, throw exception.
-                throw new FfbTableException("Unknown user!");
-            }
-        } else {
-
-            // Unknown connection, throw exception.
+    /**
+     * Create the database connection.
+     * @param string $typeConnection
+     * @param string $user
+     * @return PDO
+     * @throws FfbTableException
+     */
+    private static function createDbConnection(string $typeConnection, string $user): PDO
+    {
+        if (!in_array($typeConnection, ["main", "stats", "tests"])) {
             throw new FfbTableException("Unknown connection!");
         }
-    }
 
-    /**
-     * Getter Database.
-     * @return PDO Database.
-     */
-    protected function getDatabase(): PDO
-    {
-        return $this->db;
-    }
-
-    /**
-     * Method to call in children class to set table name.
-     * @return string Table name.
-     */
-    protected function getNameTable(): string
-    {
-        return "";
-    }
-
-    /**
-     * Getter Table name.
-     * @return string Table name.
-     */
-    protected function getTable(): string
-    {
-        return $this->table;
-    }
-
-    /**
-     * Setter Table name.
-     * @param string $table new Table name.
-     * @return void
-     */
-    protected function setTable(string $table): void
-    {
-        $this->table = $table;
-    }
-
-    /**
-     * Getter Columns names.
-     * @return array Columns names.
-     */
-    protected function getColumns(): array
-    {
-        return $this->columns;
-    }
-
-    /**
-     * Getter Columns names as string.
-     * @return string Columns names.
-     */
-    protected function getColumnsSelect(): string
-    {
-        return implode(", ", $this->getColumns());
-    }
-
-    /**
-     * Setter Columns names.
-     * @param array $columns New array of Columns names.
-     * @return void
-     */
-    protected function setColumns(array $columns): void
-    {
-        $this->columns = $columns;
-    }
-
-    /**
-     * Getter TypeConnection.
-     * @return string TypeConnection.
-     */
-    protected function getTypeConnection(): string
-    {
-        return $this->typeConnection;
-    }
-
-    /**
-     * Getter User.
-     * @return string User.
-     */
-    protected function getUser(): string
-    {
-        return $this->user;
-    }
-
-    /**
-     * Getter PropertyColumns.
-     * @return array PropertyColumns.
-     */
-    protected function getPropertiesColumns(): array
-    {
-        $propertiesColumns = [];
-
-        switch ($this->table) {
-            case "actions":
-            case "ratings":
-            case "tag_types":
-            case "scores":
-                $propertiesColumns = ["id", "name"];
-                break;
-            case "users":
-                $propertiesColumns = User::getProperties();
-                break;
-            case "authors":
-                $propertiesColumns = Author::getProperties();
-                break;
-            case "fandoms":
-                $propertiesColumns = Fandom::getProperties();
-                break;
-            case "languages":
-                $propertiesColumns = Language::getProperties();
-                break;
-            case "tags":
-                $propertiesColumns = Tag::getProperties();
-                break;
-            case "characters":
-                $propertiesColumns = Character::getProperties();
-                break;
-            case "relations":
-                $propertiesColumns = Relation::getProperties();
-                break;
-            case "fanfictions":
-                $propertiesColumns = Fanfiction::getProperties();
-                break;
-            case "links":
-                $propertiesColumns = Link::getProperties();
-                break;
-            case "series":
-                $propertiesColumns = Series::getProperties();
-                break;
+        if (!in_array($user, ["guest", "user", "admin"])) {
+            throw new FfbTableException("Unknown user!");
         }
 
-        return $propertiesColumns;
+        // If connection type is known, then use config file.
+        if (file_exists("../config/config.php"))
+            $configFile = include "../config/config.php";
+        else if (file_exists("../../config/config.php"))
+            $configFile = include "../../config/config.php";
+
+        try {
+            // Creation of Php Database Object with provided data from config.
+            return new PDO("mysql:host=" . $configFile["credentials"][$user]["host"] . ";dbname=" . $configFile["db"][$typeConnection], $configFile["credentials"][$user]["user"], $configFile["credentials"][$user]["password"]);
+        } catch (PDOException $e) {
+            // Exception caught, throw new exception with previous exception message.
+            throw new FfbTableException($e->getMessage());
+        }
     }
 }
