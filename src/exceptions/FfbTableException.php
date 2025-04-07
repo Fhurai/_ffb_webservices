20511455079580e681590b834ad55dc4dcee9213
Fhurai <kulu57@gmail.com>
2025-04-08 00:05:34 +0200

    Refactor FfbTableException class for improved formatting and consistency

--- src/exceptions/FfbTableException.php
+++ src/exceptions/FfbTableException.php
@@ -7,18 +7,20 @@
  *
  * @package exceptions
  */
-class FfbTableException extends Exception{
+class FfbTableException extends Exception
+{
 
     /**
      * Implied Constructor.
      * @param array $
      */
-    public function __construct(){
+    public function __construct()
+    {
         $args = func_get_args();
         $count = func_num_args();
 
-        if(method_exists($this,$function = "constructorWith" . $count)){
-            call_user_func_array(array($this,$function), $args);
+        if (method_exists($this, $function = "constructorWith" . $count)) {
+            call_user_func_array(array($this, $function), $args);
         }
     }
 
@@ -27,7 +29,8 @@ class FfbTableException extends Exception{
      * @param string $message Current message
      * @return void
      */
-    public function constructorWith1(string $message): void {
+    public function constructorWith1(string $message): void
+    {
         parent::__construct($message, 500, null);
     }
 
@@ -37,7 +40,8 @@ class FfbTableException extends Exception{
      * @param int $code Return code
      * @return void
      */
-    public function constructorWith2(string $message, int $code): void{
+    public function constructorWith2(string $message, int $code): void
+    {
         parent::__construct($message, $code, null);
     }
 
@@ -48,7 +52,8 @@ class FfbTableException extends Exception{
      * @param Throwable $previous Previous throwable.
      * @return void
      */
-    public function constructorWith3(string $message, int $code, Throwable $previous): void{
+    public function constructorWith3(string $message, int $code, Throwable $previous): void
+    {
         parent::__construct($message, $code, $previous);
     }
 }
