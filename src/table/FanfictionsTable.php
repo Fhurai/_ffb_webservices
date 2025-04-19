fc67c84742d5aa51f0fb07159d51853c9b1d33de
Fhurai <kulu57@gmail.com>
2025-04-19 23:09:28 +0200

    Rename this field "$primary_key" to match the regular expression ^[a-z][a-zA-Z0-9]*$

--- src/table/FanfictionsTable.php
+++ src/table/FanfictionsTable.php
@@ -19,7 +19,7 @@ require_once __DIR__ . "/../builder/TagBuilder.php";
  */
 class FanfictionsTable extends EntitiesTable
 {
-    private string $primary_key = 'fanfiction_id';
+    private string $primaryKey = 'fanfiction_id';
     protected const TABLE_NAME = 'fanfictions';
     protected const DEFAULT_SELECT_QUERY = 'SELECT * FROM `fanfictions`';
 
@@ -209,14 +209,14 @@ class FanfictionsTable extends EntitiesTable
     private function updateAssociationsLinks(int $id, array $items): void
     {
         $queryDelete = "DELETE FROM `links` WHERE `fanfiction_id` = :fanfiction_id";
-        $this->executeQuery($queryDelete, [$this->primary_key => $id]);
+        $this->executeQuery($queryDelete, [$this->primaryKey => $id]);
 
         if ($items) {
             $queryInsert = "INSERT INTO `links`(`url`, `fanfiction_id`) VALUES (:url, :fanfiction_id)";
             foreach ($items as $item) {
                 $this->executeQuery($queryInsert, [
                     ":url" => $item->getUrl(),
-                    $this->primary_key => $id
+                    $this->primaryKey => $id
                 ]);
             }
         }
