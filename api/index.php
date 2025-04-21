6b70cc3b67bcd207954e9fe5c1cca6be964ab18b
Fhurai <kulu57@gmail.com>
2025-04-21 19:59:56 +0200

    Curl check php server

---
+++ api/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Fanfiction Bookmarks API</title>
+</head>
+<body>
+    <h1>Welcome to the API</h1>
+    <p>This is the API documentation for the Fanfiction Bookmarks project.</p>
+    <p>To get started, please refer to the following endpoints:</p>
+    <ul>
+        <li><a href="https://api.fanfictionbookmarks.com/login.php">Login</a> - Authenticate and obtain a token.</li>
+        <li><a href="https://api.fanfictionbookmarks.com/scores.php">Scores</a> - Retrieve scores.</li>
+        <li><a href="https://api.fanfictionbookmarks.com/ratings.php">Ratings</a> - Retrieve ratings.</li>
+        <li><a href="https://api.fanfictionbookmarks.com/tagtypes.php">Tag Types</a> - Retrieve tag types.</li>
+        <li><a href="https://api.fanfictionbookmarks.com/actions.php">Actions</a> - Retrieve actions.</li>
+</body>
+</html>
