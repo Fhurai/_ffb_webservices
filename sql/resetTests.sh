92f55633227db3a2d3d2cc26e9acc5efd8c87ab0
Fhurai <kulu57@gmail.com>
2025-04-22 23:34:31 +0200

    Add Docker configuration files and entrypoint script for PHP application

--- sql/resetTests.sh
+++ sql/resetTests.sh
@@ -1,5 +1,2 @@
-# Print a message indicating the start of the database reset process
-echo -n "Reset db tests : " \
-&& mysql --login-path=client ffb_tests < main/0.main_drop.sql >/dev/null 2>&1 \
-&& mysqldump --login-path=client ffb_main 2>/dev/null | mysql --login-path=client ffb_tests >/dev/null 2>&1 \
-&& echo "done"
+#!/bin/bash
+echo -n "Reset db tests : " && mysql --login-path=client ffb_tests < main/0.main_drop.sql >/dev/null 2>&1 && mysqldump --login-path=client ffb_main 2>/dev/null | mysql --login-path=client ffb_tests >/dev/null 2>&1 && echo "done"
